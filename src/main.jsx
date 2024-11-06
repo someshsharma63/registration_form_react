f91802b025260dcd35b1b762f88393f0c9d3711f
Somesh <someshdevera98@gmail.com>
2024-11-06 18:54:25 +0530

    check button update and change layout of form

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
+import "bootstrap/dist/js/bootstrap.js";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
     <App />
- 
 )
